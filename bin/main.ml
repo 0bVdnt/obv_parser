fad59248f6996db4445085f3910752c635a686dc
0bVdnt <vedantneve13@gmail.com>
2025-06-10 19:16:46 +0530

    Added Recursive Descent functions

--- bin/main.ml
+++ bin/main.ml
@@ -106,3 +106,75 @@ let expect (expected : token) (tokens : token list) : token list =
     else
         let msg = "Expected " ^ (string_of_token expected) ^ " but found " ^ (string_of_token actual) in
         raise (SyntaxError msg)
+
+(* IV. Recursive Descent Parser *)
+(* Each function here corresponds to a non-terminal symbol in the grammar. *)
+
+(* parse_exp
+   Grammar: <exp> ::= <constant>
+   Consumes a constant token and returns an 'exp' AST node.
+*)
+
+let parse_exp (tokens : token list) : exp * token list =
+    let token, rest_of_tokens = take_token tokens in
+    match token with
+        | T_CONSTANT i -> (E_Constant i, rest_of_tokens)
+        | other ->
+            let msg = "Expected a constant expression but found " ^ string_of_token other in
+            raise (SyntaxError msg)
+
+(* parse_statement
+   Grammar: <statement> ::= "return" <exp> ";"
+   Consumes a return statement and returns a 'statement' AST node.
+*)
+
+let parse_statement (tokens : token list) : statement * token list =
+    let tokens_after_return = expect T_KW_RETURN tokens in
+
+    let exp_node, tokens_after_exp = parse_exp tokens_after_return in
+
+    let tokens_after_semicolon = expect T_SEMICOLON tokens_after_exp in
+
+    (S_Return exp_node, tokens_after_semicolon)
+
+(* parse_function
+   Grammar: <function> ::= "int" <identifier> "(" ")" "{" <statement> "}"
+   Consumes a full function definition and returns a 'func' AST node.
+*)
+
+let parse_function (tokens : token list) : func * token list =
+    let tokens1 = expect T_KW_INT tokens in
+
+    let (func_name, tokens) =
+        let (token, rest) = take_token tokens in
+        match token with =
+        | T_IDENTIFIER s -> (s, rest)
+        | other -> 
+            let msg = "Expected an identifier for function name, but found " ^ string_of_token other in
+            raise (SyntaxError msg)
+    in
+    let tokens = expect T_LPAREN tokens in
+    let tokens = expect T_RPAREN tokens in
+    let tokens = expect T_LBRACE tokens in
+
+    let (body_node, tokens) = parse_statement tokens in
+    let tokens = expect T_RBRACE tokens in
+    let func_node = { name = func_name; body = body_node } in
+    (func_node, tokens)
+
+(* parse_program
+   Grammar: <program> ::= <function>
+   This is the top-level entry point for parsing. It consumes an entire
+   token stream and returns a single 'program' AST node.
+*)
+
+let parse_prgram (tokens : token list) : program =
+    let (func_node, remaining_tokens) = parse_function tokens in
+    
+    match remaining_tokens with
+    | [] ->
+        P_PROGRAM func_node
+    | hd :: _ ->
+        let msg = "Expected end of file but found unexpected token: " ^ (string_of_token hd) in
+        raise(SyntaxError msg)
+
